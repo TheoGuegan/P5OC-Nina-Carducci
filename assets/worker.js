9dba2f45cfa11356dd9db1303c3412e05ca4fc89
TheoGuegan <tgdicaweb@gmail.com>
2023-04-21 10:09:04 +0200

    ajout title & compression css

--- assets/worker.js
+++ assets/worker.js
@@ -1,4 +1,5 @@
 // Enregistrement des fichiers statiques dans le cache
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('static-cache').then(function(cache) {

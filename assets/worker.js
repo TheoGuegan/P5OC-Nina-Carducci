bacb6618f643dadf7852c4196da15fa2cee740bb
TheoGuegan <tgdicaweb@gmail.com>
2023-04-21 10:37:46 +0200

    correction worker.js (enregistrement cache)

--- assets/worker.js
+++ assets/worker.js
@@ -12,7 +12,18 @@ self.addEventListener('install', function(event) {
         './assets/images/slider/nicholas-green.webp',
         './assets/images/slider/edward-cisneros.webp',
         './assets/images/nina-photopres.webp',
-        './assets/images/instagram-min.webp'
+        './assets/images/instagram-min.webp',
+        './assets/images/camera.webp',
+        './assets/images/gallery/concerts/aaron-paul.webp',
+        './assets/images/gallery/concerts/austin-neill.webp',
+        './assets/images/gallery/entreprise/ali-morshedlou.webp',
+        './assets/images/gallery/entreprise/jason-goodman.webp',
+        './assets/images/gallery/entreprise/mateus-campos.webp',
+        './assets/images/gallery/mariage/hannah-busing.webp',
+        './assets/images/gallery/mariage/jakob-owens.webp',
+        './assets/images/gallery/portraits/ade-tunji.webp',
+        './assets/images/gallery/portraits/nino-van-prattenburg.webp',
+        
       ]);
     })
   );
